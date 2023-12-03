6b619fae574c51b84a207e88ad96d347f2531062
GreatInfernal <120935775+GreatInfernal@users.noreply.github.com>
2023-12-02 23:04:31 -0500

    Update Part2.cpp
    Code similar to that of part 1, 
    added zero to nine numbers to comparison, and saved the index of matching number
    Found required integer by adding 10 to vector index. Would use dictionary for larger set.

--- Advent of Code/Solutions/Day1/Part2.cpp
+++ Advent of Code/Solutions/Day1/Part2.cpp
@@ -2,36 +2,48 @@
 #include <vector>
 #include <string>
 #include <fstream>
+
+int readLine(std::string& line){
+    std::vector<std::string> numbers={
+        "zero","one","two","three","four","five","six","seven","eight","nine",
+        "0","1","2","3","4","5","6","7","8","9"};
+    int firs=line.length();
+    int last=-1;
+    int fd,ld;
+    for(int i=0;i<numbers.size();i++){
+        auto a=numbers[i];
+        int pos=line.find(a);
+        if(pos!=std::string::npos){
+            if(pos<firs){
+                firs=pos;
+                fd=i;
+            }
+        }
+        pos=line.rfind(a);
+         if(pos!=std::string::npos){
+            if(pos>last){
+                last=pos;
+                ld=i;
+            }
+        }
+    }
+    if(fd<10){
+        fd=fd+10;
+    }
+    if(ld<10){
+        ld=ld+10;
+    }
+    return std::stoi(numbers[fd]+numbers[ld]);
+}
+
+
 int main(){
     std::ifstream data;
     data.open("./../../inputs/Day1/input.txt");
-    std::vector<std::pair<std::string,std::string>> nums={
-        std::make_pair("zero","z0o"),
-        std::make_pair("one","o1e"),
-        std::make_pair("two","t2o"),
-        std::make_pair("three","t3e"),
-        std::make_pair("four","f4r"),
-        std::make_pair("five","f5e"),
-        std::make_pair("six","s6x"),
-        std::make_pair("seven","s7n"),
-        std::make_pair("eight","e8t"),
-        std::make_pair("nine","n9e"),
-
-    };
     std::string line;
     int sum=0;
     while(std::getline(data,line)){
-        for(int i=0;i<nums.size();i++){
-            int pos=0;
-            while (line.find(nums[i].first,pos)!=std::string::npos){
-                line.replace(line.find(nums[i].first,pos),nums[i].first.length(),nums[i].second);
-                pos=line.find(nums[i].first,pos);
-            }
-        }
-        char a=line[line.find_first_of("0123456789")];
-        char b=line[line.find_last_of("0123456789")];
-        int n=(a-'0')*10+b-'0';
-         sum=sum+n;
+        sum=sum+readLine(line);
     }
     std::cout<<"Sum: "<<sum;
     return 0;
