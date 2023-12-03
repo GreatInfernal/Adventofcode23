56d7836f5acc7fc89a09db1a8a17025684472665
GreatInfernal <120935775+GreatInfernal@users.noreply.github.com>
2023-12-02 23:03:02 -0500

    Update Part1.cpp
    Utilized function to parse lines.
    search from a vector of numbers to find first and last match. Can be optimized by lowering find parameters.

--- Advent of Code/Solutions/Day1/Part1.cpp
+++ Advent of Code/Solutions/Day1/Part1.cpp
@@ -2,16 +2,39 @@
 #include <vector>
 #include <string>
 #include <fstream>
+
+int readLine(std::string& line){
+    std::vector<std::string> numbers={"0","1","2","3","4","5","6","7","8","9"};
+    int firs=line.length();
+    int last=-1;
+    std::string fd,ld;
+    for(auto a:numbers){
+        int pos=line.find(a);
+        if(pos!=std::string::npos){
+            if(pos<firs){
+                firs=pos;
+                fd=a;
+            }
+        }
+        pos=line.rfind(a);
+         if(pos!=std::string::npos){
+            if(pos>last){
+                last=pos;
+                ld=a;
+            }
+        }
+    }
+    return std::stoi(fd+ld);
+}
+
+
 int main(){
     std::ifstream data;
     data.open("./../../inputs/Day1/input.txt");
     std::string line;
     int sum=0;
     while(std::getline(data,line)){
-            char a=line[line.find_first_of("0123456789")];
-            char b=line[line.find_last_of("0123456789")];
-            int n=(a-'0')*10+b-'0';
-            sum=sum+n;
+        sum=sum+readLine(line);
     }
     std::cout<<"Sum: "<<sum;
     return 0;
